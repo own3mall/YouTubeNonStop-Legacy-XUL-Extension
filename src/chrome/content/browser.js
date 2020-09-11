9c1d1226181fcbb89e28ee530db835343381c888
OwN-3m-All <own3mall@gmail.com>
2020-09-11 17:29:39 -0600

    Better Existence Handling

--- src/chrome/content/browser.js
+++ src/chrome/content/browser.js
@@ -25,6 +25,8 @@ var foundButton;
 var ytVideo = null;
 var currentTab = null;
 var shouldSwitchTab = false;
+var initEvent = null;
+var isTypingInSearchBox = false;
 
 /* Functions */
 
@@ -42,40 +44,58 @@ var YouTubeNonStopObj = {
 				gBrowser.addEventListener("load", YouTubeNonStopObj.initCore, true);
 			}
 		}, false);
+		
+		var searchBar = window.document.getElementById("searchbar");
+		if(typeof searchBar !== 'undefined' && searchBar !== null){
+			searchBar.onfocus = function() {
+				isTypingInSearchBox = true;
+				log("YouTubeNonStop: Search bar in use.");
+			};
+			searchBar.onblur = function() {
+				isTypingInSearchBox = false;
+				log("YouTubeNonStop: Search bar no longer being used.");
+			};
+		}
 	},
 	initCore: function(event){
-		// this is the content document of the loaded page.
-		let doc = event.originalTarget;
+		//initEvent = event;
+		//log(initEvent);
+		
+		if(typeof event.originalTarget !== 'undefined' && typeof event.originalTarget.defaultView !== 'undefined'){
+		
+			// this is the content document of the loaded page.
+			let doc = event.originalTarget;
 
-		if (doc instanceof HTMLDocument) {
-			// is this an inner frame?
-			if (doc.defaultView.frameElement) {
-				// Frame within a tab was loaded.
-				// Find the root document:
-				while (doc.defaultView.frameElement) {
-					doc = doc.defaultView.frameElement.ownerDocument;
+			if (doc instanceof HTMLDocument) {
+				// is this an inner frame?
+				if (doc.defaultView.frameElement) {
+					// Frame within a tab was loaded.
+					// Find the root document:
+					while (doc.defaultView.frameElement) {
+						doc = doc.defaultView.frameElement.ownerDocument;
+					}
 				}
 			}
-		}
-					
-		var dmn = doc.defaultView.location.href;
-				  
-		// log("YouTubeNonStop: Page domain is " + dmn);
-		if(dmn.match(matchStr) || debug){
-			if(gBrowser.getBrowserForTab(gBrowser.selectedTab).contentDocument.URL.match(matchStr) || debug){
-				foundUrlIndex = arrayUrls.indexOf(gBrowser.getBrowserForTab(gBrowser.selectedTab).contentDocument.URL)
-				if(foundUrlIndex != -1){
-					if(extraInfo){
-						log("YouTubeNonStop: YouTube duplicate instance detected. Removing previous reference!");
+						
+			var dmn = doc.defaultView.location.href;
+					  
+			// log("YouTubeNonStop: Page domain is " + dmn);
+			if(dmn.match(matchStr) || debug){
+				if(gBrowser.getBrowserForTab(gBrowser.selectedTab).contentDocument.URL.match(matchStr) || debug){
+					foundUrlIndex = arrayUrls.indexOf(gBrowser.getBrowserForTab(gBrowser.selectedTab).contentDocument.URL)
+					if(foundUrlIndex != -1){
+						if(extraInfo){
+							log("YouTubeNonStop: YouTube duplicate instance detected. Removing previous reference!");
+						}
+						YouTubeNonStopObj.removeDup(gBrowser.getBrowserForTab(gBrowser.selectedTab).contentDocument.URL, foundUrlIndex);
 					}
-					YouTubeNonStopObj.removeDup(gBrowser.getBrowserForTab(gBrowser.selectedTab).contentDocument.URL, foundUrlIndex);
+						
+					arrayOfDoms.push(gBrowser.getBrowserForTab(gBrowser.selectedTab).contentDocument);
+					arrayOfTabs.push(gBrowser.selectedTab);
+					arrayUrls.push(gBrowser.getBrowserForTab(gBrowser.selectedTab).contentDocument.URL);
+					log("YouTubeNonStop: YouTube detected on URL \"" + gBrowser.getBrowserForTab(gBrowser.selectedTab).contentDocument.URL + "\". Running monitoring code to detect pause notification!");
+					YouTubeNonStopObj.youTubeMonitor();
 				}
-					
-				arrayOfDoms.push(gBrowser.getBrowserForTab(gBrowser.selectedTab).contentDocument);
-				arrayOfTabs.push(gBrowser.selectedTab);
-				arrayUrls.push(gBrowser.getBrowserForTab(gBrowser.selectedTab).contentDocument.URL);
-				log("YouTubeNonStop: YouTube detected. Running monitoring code to detect pause notification!");
-				YouTubeNonStopObj.youTubeMonitor();
 			}
 		}
 	},
@@ -91,7 +111,7 @@ var YouTubeNonStopObj = {
 		toRemove = new Array();
 
 		// Only perform the check if the URL and search bar are not focused!!!!
-		if(!gURLBar.focused){
+		if(!gURLBar.focused && !isTypingInSearchBox){
 			if(extraInfo){
 				dateNow = new Date();
 				log("YouTubeNonStop: Checking for pause button on " + dateNow.toLocaleDateString() + " " + dateNow.toLocaleTimeString('en-US'));
